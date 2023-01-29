42ccb4f54eb0dca437b63760eae0e86f39d783ef
Ryo Ueda <u483198f@gmail.com>
2023-01-30 00:24:33 +0900

    deploy

---
+++ cloud_functions/main.py
@@ -0,0 +1,41 @@
+import tweepy
+import os
+import requests
+import json
+
+# from dotenv import load_dotenv
+
+# load_dotenv(verbose=True)
+API_KEY = os.environ["API_KEY"]
+API_SECRET = os.environ["API_SECRET"]
+BEARER_TOKEN = os.environ["BEARER_TOKEN"]
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
+WEATHER_API_KEY = os.environ["WEATHER_API_KEY"]
+
+
+def post_tweet(request):
+
+    client = tweepy.Client(
+        bearer_token=BEARER_TOKEN,
+        consumer_key=API_KEY,
+        consumer_secret=API_SECRET,
+        access_token=ACCESS_TOKEN,
+        access_token_secret=ACCESS_TOKEN_SECRET,
+    )
+    response = requests.get(
+        "https://api.openweathermap.org/data/2.5/weather",
+        params={
+            "q": "yakutsk",
+            "appid": WEATHER_API_KEY,
+            "units": "metric",
+            "lang": "ja",
+        },
+    )
+    response_text = json.loads(response.text)
+    weather = response_text["weather"][0]["description"]
+    temp = response_text["main"]["temp"]
+    humidity = response_text["main"]["humidity"]
+    message = f"現在のヤクーツク\n天気: {weather} \n気温: {temp}\n湿度: {humidity}"
+    client.create_tweet(text=message)
+    return ""
