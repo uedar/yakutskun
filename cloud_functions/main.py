e082a641f44929010410e26d7519a2fe7ed04b4f
Ryo Ueda <u483198f@gmail.com>
2023-01-31 19:29:48 +0900

    change api

--- cloud_functions/main.py
+++ cloud_functions/main.py
@@ -4,9 +4,9 @@ import requests
 import json
 from datetime import datetime, timedelta, timezone
 
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv(verbose=True)
+load_dotenv(verbose=True)
 
 API_KEY = os.environ["API_KEY"]
 API_SECRET = os.environ["API_SECRET"]
@@ -26,23 +26,29 @@ def post_tweet(request):
         access_token_secret=ACCESS_TOKEN_SECRET,
     )
     response = requests.get(
-        "https://api.openweathermap.org/data/2.5/weather",
+        "https://api.openweathermap.org/data/3.0/onecall",
         params={
-            "q": "yakutsk",
+            "lat": 62.0397,
+            "lon": 129.7422,
+            "exclude": "current,minutely,daily,alerts",
             "appid": WEATHER_API_KEY,
             "units": "metric",
             "lang": "ja",
         },
     )
-    JST = timezone(timedelta(hours=+9), "YAKT")
-    dt_now = datetime.now(JST)
+    weather_json = json.loads(response.text)
+    weather_info = weather_json["hourly"][0]
+    YAKT = timezone(timedelta(hours=+9), "YAKT")
+    dt_now = datetime.now(YAKT)
     now = dt_now.strftime("%Y/%m/%d %H:%M")
-    weather_info = json.loads(response.text)
-    weather = weather_info["weather"][0]["description"]
-    temperature = weather_info["main"]["temp"]
-    humidity = weather_info["main"]["humidity"]
-    message = (
-        f"{now} (YAKT) のヤクーツク\n天気: {weather} \n気温: {temperature}℃\n湿度: {humidity}%"
-    )
+    japanese_weather_list = ["晴れ", "曇り", "雨", "雪"]
+    english_weather_list = ["Clear", "Clouds", "Rain", "Snow"]
+    weather = weather_info["weather"][0]["main"]
+    if weather in english_weather_list:
+        weather = japanese_weather_list[english_weather_list.index(weather)]
+    temperature = weather_info["temp"]
+    feel_like = weather_info["feels_like"]
+    humidity = weather_info["humidity"]
+    message = f"{now} (YAKT) のヤクーツク\n天気: {weather} \n気温: {temperature}℃\n体感温度: {feel_like}℃\n湿度: {humidity}%"
     post_request = client.create_tweet(text=message)
     return post_request.data
